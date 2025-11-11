beb9ae3bd09a556420d35b0995d325bfb64af895
Rittik Chawla <rittik.ch30516@gmail.com>
2025-11-11 20:41:23 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ const downloadCV = () => {
                         <span className="up">HI!</span>
                         <span className="down">I am Rittik Chawla</span>
                     </h1>
-                    <p className="header-subtitle">FRONTEND WEB DESIGNER</p>
+                    <p className="header-subtitle">Software Developer</p>
 
                     <button className="btn btn-primary" id="downloadButton" onClick={downloadCV}>Download CV</button>
 
